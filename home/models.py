0570a443a50363f5b1632a44119e84652424dd6e
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-11-05 14:28:20 +0100

    minor tag fix

--- home/models.py
+++ home/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-
 from django_kapnoc.models import MarkdownPage
+
+from blog.models import Tag
 
 
 class HomePage(MarkdownPage):
+    tags = models.ManyToManyField(Tag)
+
     class Meta:
         app_label = 'home'
