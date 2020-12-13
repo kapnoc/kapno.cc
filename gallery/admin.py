bea72c9dd252640240c97782d2ea60da00dbc858
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-12-13 21:16:21 +0100

    WIP: GalleryEntry admin thumbnail

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from django.utils.safestring import mark_safe
 from easy_thumbnails.files import get_thumbnailer
 
@@ -11,10 +12,25 @@ class ImageAdmin(admin.ModelAdmin):
     search_fields = ('name_unique', 'description')
     list_per_page = 25
 
+    @mark_safe
     def image_display(self, obj):
-        return mark_safe('<a href="{0}"><img src="{0}"></a>'.format(get_thumbnailer(obj.image)['admin_small'].url))
+        return '<a href="{0}"><img src="{0}"></a>'.format(get_thumbnailer(obj.image)['admin_small'].url)
 
 
-admin.site.register(GalleryEntry)
+class ImageChoiceField(forms.ModelChoiceField):
+    @mark_safe
+    def label_from_instance(self, obj):
+        thumb = get_thumbnailer(obj.image)["admin_small"].url
+        return f'<p>{obj.name_unique}: <img src="{thumb}"/></p>'
+
+
+class GalleryEntryAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'image':
+            return ImageChoiceField(queryset=Image.objects.all())
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(GalleryEntry, GalleryEntryAdmin)
 admin.site.unregister(Image)
 admin.site.register(Image, ImageAdmin)
