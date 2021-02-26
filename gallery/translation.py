f1f735ffc489216758e7a231e5b62f8910e4f87e
Tanguy Gérôme <tanguy.gerome@gmail.com>
2021-02-26 22:56:55 +0100

    temporary gallery fix: no translation

--- gallery/translation.py
+++ gallery/translation.py
@@ -1,13 +1,14 @@
 from modeltranslation.translator import register, TranslationOptions
 from photologue.models import Gallery, Photo
-# from .models import GalleryEntry
 
 
-@register(Gallery)
-class GalleryTranslationOptions(TranslationOptions):
-    fields = ('title', 'description',)
+# TODO: the translation fields are not created by makemigrations, to be fixed
+
+# @register(Gallery)
+# class GalleryTranslationOptions(TranslationOptions):
+#     fields = ('title', 'description',)
 
 
-@register(Photo)
-class PhotoTranslationOptions(TranslationOptions):
-    fields = ('title', 'caption',)
+# @register(Photo)
+# class PhotoTranslationOptions(TranslationOptions):
+#     fields = ('title', 'caption',)
